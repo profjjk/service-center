86dac7e9934063bbf2f704171aa269b155078135
Jordan Kelly <profjjk@gmail.com>
2023-03-06 12:16:58 -0800

    add create function to jobs hook

--- client/src/pages/customers/index.jsx
+++ client/src/pages/customers/index.jsx
@@ -1,53 +1,51 @@
-import { useCustomers } from './hooks/useCustomers';
 import { useNavigate } from 'react-router-dom';
-import { useState } from 'react';
-import { useUser, withSearch, CustomerForm } from '../../components';
+import { useCustomers } from './hooks/useCustomers';
+import { useUser, withFilter } from '../../components';
 import { Table } from '../../layouts';
 import { Searchbar } from '../../features';
 import './style.scss';
 
-const Customers = ({ search, setSearch }) => {
-    const [selectedCustomer, setSelectedCustomer] = useState(null);
+const Customers = ({ filter, setFilter }) => {
     const { user } = useUser();
     const { data: customers } = useCustomers();
     const navigate = useNavigate();
 
     if (!user) navigate('/auth');
 
-    const tableHeaders = ['Business Name', 'Address', 'Contact', 'Phone #'];
-
-    const applySearchFilter = (customers) => {
+    const applyFilter = (customers) => {
         return customers.filter((c) => (
-            c?.businessName.toLowerCase().includes(search.toLowerCase()) ||
-            c?.address?.city.toLowerCase().includes(search.toLowerCase()) ||
-            c?.phone.includes(search)
+            c?.businessName.toLowerCase().includes(filter.toLowerCase()) ||
+            c?.address?.city.toLowerCase().includes(filter.toLowerCase()) ||
+            c?.phone.includes(filter)
         ))
     }
 
     return (
         <main>
             <h1>Customers Page</h1>
-            {!selectedCustomer && customers &&
-                <section>
+            {typeof filter !== 'object' && customers &&
+                <section className={'table'}>
                     <Searchbar
+                        setFilter={setFilter}
                         placeholder={'Search business name, city, or phone #'}
-                        setSearch={setSearch}
                     />
                     <Table
-                        headers={tableHeaders}
-                        rows={applySearchFilter(customers)}
+                        setFilter={setFilter}
+                        headers={['Business Name', 'Address', 'Contact', 'Phone #']}
+                        rows={applyFilter(customers)}
                         type={'customer'}
                     />
+                    {customers.length < 1 && <p className={'empty-list'}>** No customers to display **</p>}
                 </section>
             }
 
-            {selectedCustomer && <CustomerForm
-                customer={selectedCustomer}
-                setSelectedCustomer={setSelectedCustomer}
-            />}
+            {/*{selectedCustomer && <CustomerForm*/}
+            {/*    customer={selectedCustomer}*/}
+            {/*    setSelectedCustomer={setSelectedCustomer}*/}
+            {/*/>}*/}
         </main>
 
     )
 }
 
-export default withSearch(Customers);
+export default withFilter(Customers);
