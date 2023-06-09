71c1ce3bb3ca3100bf5d68a176dfe312575bdcf7
Jordan Kelly <profjjk@gmail.com>
2023-06-09 00:03:46 -0700

    update home page

--- client/src/pages/home/index.jsx
+++ client/src/pages/home/index.jsx
@@ -1,7 +1,39 @@
+import { useAuth } from '../../components';
+import { useNavigate } from 'react-router-dom';
+import './style.scss';
+
 export const Home = () => {
+    const { login } = useAuth();
+    const navigate = useNavigate();
+
+    const loginDemo = async (e) => {
+        e.preventDefault();
+        const demoCreds = {
+            email: 'demo@demo.com',
+            password: 'password'
+        };
+        try {
+            const authorization = await login(demoCreds);
+            if (authorization) {
+                navigate('/dashboard');
+            }
+        } catch (err) {
+            console.error(err);
+        }
+
+    };
+
     return (
-        <main>
-            <h1>Home Page</h1>
+        <main id={'landing-page'}>
+            <h1>A simple and easy solution <br/> to help manage your daily business.</h1>
+
+            <div>
+                <p id={'description'}>
+                    Boost efficiency and simplify operations with a comprehensive web application. Manage customers,
+                    track service jobs, and lookup invoices.
+                </p>
+                <p id={'demo-login'} onClick={loginDemo}>Try out the demo &rarr;</p>
+            </div>
         </main>
     );
 };
