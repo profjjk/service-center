65e84d6be7e1f974a7c42bd22f2d29045ff053b0
Jordan Kelly <profjjk@gmail.com>
2023-06-23 10:00:10 -0700

    set deployment api url

--- client/src/lib/axiosInstance/index.js
+++ client/src/lib/axiosInstance/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-let baseUrl = 'http://localhost:8080/api';
+let baseUrl = 'https://service-center-server-c06036aa6434.herokuapp.com/';
 
 const config = { baseURL: baseUrl };
 
